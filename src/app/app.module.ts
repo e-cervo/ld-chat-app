c421dc6d7d0b4e581df174eb6582157c10d59d0d
Jonas Kistler <jonas.kistler@e-cervo.com>
2022-07-07 11:35:48 +0200

    Magnifique matinput

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './ld-chat/components/chat/chat.component';
 import { LdChatModule } from './ld-chat/ld-chat.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ChatComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    LdChatModule],
+    LdChatModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule,
+    MatButtonModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
